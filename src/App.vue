baee5fd8b652413adc4127a93649e79904c73172
Andy Bulka <abulka@gmail.com>
2024-12-25 17:51:48 +1100

    initial

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import GridStackDemo1 from './components/GridStackDemo1.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <GridStackDemo1 />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
