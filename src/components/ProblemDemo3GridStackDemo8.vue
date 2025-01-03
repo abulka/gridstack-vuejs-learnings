9cb2f4d2e97b5bf58ada6528c379a04a30ee6af1
Andy Bulka <abulka@gmail.com>
2025-01-03 14:36:31 +1100

    doco on computed hacks

--- src/components/ProblemDemo3GridStackDemo8.vue
+++ src/components/ProblemDemo3GridStackDemo8.vue
@@ -4,6 +4,7 @@
     <p v-if="selectedGridStackNode">selectedGridStackNode.x: {{ selectedGridStackNode?.x }} y: {{
         selectedGridStackNode?.y }}</p>
     <p v-else>no node selected</p>
+    <!-- <p>updateTrigger {{ updateTrigger }}</p> -->
     <button type="button"
         @click="addNewWidget">Add Widget</button>
     <div class="grid-stack"
@@ -25,11 +26,46 @@ const items = [
     { x: 0, y: 6, w: 2, h: 2, id: '5' },
 ];
 const selectedId = ref('');
+const updateTrigger = ref(0)
 
+/**
+ * This computed is the key to reactivity. It is triggered by changing the
+ * selectedId value. It returns a new object to ensure the UI is re-rendered.
+ *
+ * Three things to watch out for:
+ * 1. When using javascript 'find' that refers to a reactive property, a reactivity change
+ *    is not detected, even if you return a new object. Need to have an explicit
+ *    dependency outside the find() call e.g. const currentSelectedId =
+ *    selectedId.value. No need to return a new object.
+ * 2. Using a fake trigger e.g. updateTrigger.value to cause the computed to
+ *    re-run (partial solution)
+ * 3. Even with a fake trigger, the computed must return a new object to trigger
+ *    the UI to re-render P.S. A slight workaround is to actually render the
+ *    updateTrigger value in the template (in which case everything re-renders,
+ *    an incidentally the selectedGridStackNode IS re-rendered - but that is a
+ *    bit of luck and a hack).
+ */
+
+// Solution 1
+// const selectedGridStackNode = computed<GridStackNode | null>(() => {
+//     console.log(`  selectedGridStackNode being computed`);
+//     const currentSelectedId = selectedId.value;  // explicit dependency NEEDED due to use of find()
+//     const result = grid?.engine.nodes.find(node => node.id === currentSelectedId) || null;
+//     return result // no need to return a new object
+// });
+
+// Solution 2
 const selectedGridStackNode = computed<GridStackNode | null>(() => {
     console.log(`  selectedGridStackNode being computed`);
-    const currentSelectedId = selectedId.value;  // explicit dependency NEEDED!!
-    return grid?.engine.nodes.find(node => node.id === currentSelectedId) || null;
+
+    updateTrigger.value;  // explicit dependency on fake trigger NEEDED!!
+    const currentSelectedId = selectedId.value;  // explicit dependency NEEDED due to use of find()
+
+    const result = grid?.engine.nodes.find(node => node.id === currentSelectedId) || null;
+
+    // even though this computed may finally trigger ok, its not actually
+    // re-rendering the UI unless we return a new object
+    return { x: result?.x, y: result?.y, id: result?.id };  // this is a new object
 });
 
 onMounted(() => {
@@ -42,10 +78,35 @@ onMounted(() => {
     });
     nextId.value = items.length + 1;
 
+    // The Problem: ❌ The User interaction of moving a widget, or calling update() to programmatically 
+    // move a widget, doesn't trigger the computed selectedGridStackNode to re-run because the gridstack
+    // object is not reactive. Thus we don't see the x, y, id of the selectedGridStackNode update in the UI.
+    // 
+    // So we intercept the 'change' event and do something here to trigger the computed.
+
+    // solution 1 - to trigger the computed - WORKS
+    // grid.on("change", function (event, items) {
+    //     console.log("change", items);
+    //     // solution 1 - WORKS
+    //     selectedId.value = '';
+    //     selectedId.value = items[0].id || '';
+    // });
+
+    // solution 1A - fails
+    // grid.on("change", function (event, items) {
+    //     console.log("change", items);
+    //     // Attempt to set selectedId to a new 'version' of the same value, like a new object - but
+    //     // for a string? DOESN'T WORK
+    //     selectedId.value = items[0].id ? `${items[0].id}` : '';
+    // });
+
+    // solution 2 - fake trigger - WORKS
     grid.on("change", function (event, items) {
         console.log("change", items);
-        // ❌ this doesn't trigger the computed
+        // The computed must ALSO return a new object to trigger the UI to re-render.
+        updateTrigger.value++;
     });
+
     grid.on("dragstop", function (event, element) {
         const node = element.gridstackNode;
     });
